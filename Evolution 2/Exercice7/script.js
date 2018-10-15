86f66e9ac929951a00f55aa022a3d4d65e9289cf
BailleuxThomas <thomasbailleux2@gmail.com>
2018-10-15 15:27:09 +0200

    Mise à jours

---
+++ Evolution 2/Exercice7/script.js
@@ -0,0 +1,19 @@
+function faireChoix() {
+faireChoix = prompt("Choose a number between 1 and 4", "1, 2, 3 or 4")
+switch (faireChoix)
+{
+    case "1":
+        alert("You typed in a 1");
+        break;
+    case "2":
+        alert("You typed in a 2");
+        break;
+    case "3":
+        alert("You typed in a 3");
+        break;
+    case "4":
+        alert("You typed in a 4");
+        break;
+}
+}
+// http://www.java2s.com/Tutorial/JavaScript/0060__Statement/Switchstatementwithreturnvaluefromapromptdialog.htm
---
+++ Evolution 2/Exercice9/script.js
@@ -0,0 +1,12 @@
+function testWhile() {
+    while (true) {
+        testWhile = prompt('Entrez quelque chose de correct');
+        if (testWhile) { // On regarde s'il y a quelque chose
+        break;
+    }
+}
+}
+
+
+
+// http://odyssey.sdlm.be/javascript/16/prompt.htm
